e0f569ee8103c08573a96bd53ec1ebe2a640e497
Alberto Biolchi <169494833+Alberto23B@users.noreply.github.com>
2024-12-28 21:55:46 +0100

    Fix navButtons

--- src/components/NavButtons.js
+++ src/components/NavButtons.js
@@ -2,10 +2,14 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default function NavButtons() {
-    return (
-        <div className="flex-row justify-around hidden w-full py-10 md:flex ">
-            <button className=""><Link className="block w-full p-5 px-24 growing card-green" to="mailto:biolchi.alberto23@gmail.com">Contact me</Link></button>
-            <button className=""><Link className="block p-5 px-24 growing card-green" to="/projects">Projects</Link></button>
-        </div>
-    )
+  return (
+    <div className="flex-row justify-around hidden w-full py-10 md:flex ">
+      <button className="block w-1/3 p-5 growing card-green">
+        <Link to="mailto:biolchi.alberto23@gmail.com">Contact me</Link>
+      </button>
+      <button className="block w-1/3 p-5 growing card-green">
+        <Link to="/projects">Projects</Link>
+      </button>
+    </div>
+  );
 }
