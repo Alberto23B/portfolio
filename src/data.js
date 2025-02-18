4f2bec6d46f71892f08763be90076a9c6e0dcc5e
Alberto Biolchi <169494833+Alberto23B@users.noreply.github.com>
2025-02-18 21:45:10 +0100

    Refactor project and certificate entries to use single quotes for consistency

--- src/data.js
+++ src/data.js
@@ -1,69 +1,69 @@
 export const projects = [
   {
-    name: "Shelfable",
+    name: 'Shelfable',
     url: [
-      "https://shelfable.vercel.app/",
-      "https://github.com/Alberto23B/shelfable",
+      'https://shelfable.vercel.app/',
+      'https://github.com/Alberto23B/shelfable',
     ],
     description: `Books search engine with authentication and data store.
         Stack: Vite/React, Tailwind, Vercel - Express, Passport, MongoDB`,
     deployed: true,
   },
   {
-    name: "Jumpstarter",
+    name: 'Jumpstarter',
     url: [
-      "https://jstarter.netlify.app/",
-      "https://github.com/Alberto23B/jumpstarter_CA",
+      'https://jstarter.netlify.app/',
+      'https://github.com/Alberto23B/jumpstarter_CA',
     ],
     description: `React-Redux project styled with TailwindCSS.
         -Weather, Quote, Background API and Task Manager `,
     deployed: true,
   },
   {
-    name: "Appointment Planner",
+    name: 'Appointment Planner',
     url: [
-      "https://appointment-planner-starting-v18.vercel.app/contacts",
-      "https://github.com/Alberto23B/CA_appointment_planner",
+      'https://appointment-planner-starting-v18.vercel.app/contacts',
+      'https://github.com/Alberto23B/CA_appointment_planner',
     ],
     description: `Portfolio project from CodeAcademy. Functionalities are validation of inputs and check on duplicates`,
     deployed: true,
   },
   {
-    name: "Dynamic Css Login",
+    name: 'Dynamic Css Login',
     url: [
-      "https://dynamic-css-login.vercel.app/",
-      "https://github.com/Alberto23B/dynamic_css_login",
+      'https://dynamic-css-login.vercel.app/',
+      'https://github.com/Alberto23B/dynamic_css_login',
     ],
     description: `React hooks and props, CSS`,
     deployed: true,
   },
   {
-    name: "fCC Exercise Tracker",
-    url: ["https://github.com/Alberto23B/Exercise-Tracker-proj"],
+    name: 'fCC Exercise Tracker',
+    url: ['https://github.com/Alberto23B/Exercise-Tracker-proj'],
     description: `API that stores an exercise log using POST request and mongoDB, and returns logs based on the user GET request`,
     deployed: false,
   },
   {
-    name: "fCC Timestamp Microcervice",
-    url: ["https://github.com/Alberto23B/fcc_timestamp_project"],
+    name: 'fCC Timestamp Microcervice',
+    url: ['https://github.com/Alberto23B/fcc_timestamp_project'],
     description: `API that returns a JSON with either a unix timestamp or a utc timestamp depending on the query params.`,
     deployed: false,
   },
   {
-    name: "fCC Header Parser",
-    url: ["https://github.com/Alberto23B/Header-Parser-project"],
+    name: 'fCC Header Parser',
+    url: ['https://github.com/Alberto23B/Header-Parser-project'],
     description: `API that returns the JSON format of the request`,
     deployed: false,
   },
   {
-    name: "fCC URL Shortener Service",
-    url: ["https://github.com/Alberto23B/Url-Shortener-Project"],
+    name: 'fCC URL Shortener Service',
+    url: ['https://github.com/Alberto23B/Url-Shortener-Project'],
     description: `API meant to POST a website and give back a shortcut URL to that website`,
     deployed: false,
   },
   {
-    name: "fCC File Metadata",
-    url: ["https://github.com/Alberto23B/File-Metadata-proj"],
+    name: 'fCC File Metadata',
+    url: ['https://github.com/Alberto23B/File-Metadata-proj'],
     description: `Upload a file to this API and get back file's metadata`,
     deployed: false,
   },
@@ -71,56 +71,60 @@ export const projects = [
 
 export const certificates = [
   {
-    name: "Create a Back-End App with JavaScript",
-    url: "https://www.codecademy.com/profiles/AlbertoBiolchi/certificates/5cb678013709022f5b908913",
+    name: 'Learn TypeScript',
+    url: 'https://www.codecademy.com/profiles/AlbertoBiolchi/certificates/56fb1e71303e37b643bb1905f31c8a09',
   },
   {
-    name: "Create an Advanced Web App with React and Redux",
-    url: "https://www.codecademy.com/profiles/AlbertoBiolchi/certificates/60198396cd6fb3000e88e8e4",
+    name: 'Create a Back-End App with JavaScript',
+    url: 'https://www.codecademy.com/profiles/AlbertoBiolchi/certificates/5cb678013709022f5b908913',
   },
   {
-    name: "fCC Javascript Algorithms and Data Structures (Beta)",
-    url: "https://www.freecodecamp.org/certification/Alberto_Biolchi/javascript-algorithms-and-data-structures-v8",
+    name: 'Create an Advanced Web App with React and Redux',
+    url: 'https://www.codecademy.com/profiles/AlbertoBiolchi/certificates/60198396cd6fb3000e88e8e4',
+  },
+  {
+    name: 'fCC Javascript Algorithms and Data Structures (Beta)',
+    url: 'https://www.freecodecamp.org/certification/Alberto_Biolchi/javascript-algorithms-and-data-structures-v8',
   },
   {
     name: "fCC Backend and API's",
-    url: "https://www.freecodecamp.org/certification/Alberto_Biolchi/back-end-development-and-apis",
+    url: 'https://www.freecodecamp.org/certification/Alberto_Biolchi/back-end-development-and-apis',
   },
   {
-    name: "CodeAcademy Learn React",
-    url: "https://www.codecademy.com/profiles/AlbertoBiolchi/certificates/af00e5032d0a68cc84879983f5d8333b",
+    name: 'CodeAcademy Learn React',
+    url: 'https://www.codecademy.com/profiles/AlbertoBiolchi/certificates/af00e5032d0a68cc84879983f5d8333b',
   },
   {
-    name: "CodeAcademy Learn React: Introduction",
-    url: "https://www.codecademy.com/profiles/AlbertoBiolchi/certificates/1bf3e70ae92b43c2a3add66cbfaec661",
+    name: 'CodeAcademy Learn React: Introduction',
+    url: 'https://www.codecademy.com/profiles/AlbertoBiolchi/certificates/1bf3e70ae92b43c2a3add66cbfaec661',
   },
   {
-    name: "CodeAcademy Learn Testing for Web Development: Fundamentals",
-    url: "https://www.codecademy.com/profiles/AlbertoBiolchi/certificates/4801a1bd0f7a449e93ea55b8ff845095",
+    name: 'CodeAcademy Learn Testing for Web Development: Fundamentals',
+    url: 'https://www.codecademy.com/profiles/AlbertoBiolchi/certificates/4801a1bd0f7a449e93ea55b8ff845095',
   },
   {
-    name: "CodeAcademy Build a Website with HTML, CSS, and GitHub Pages Skill Path",
-    url: "https://www.codecademy.com/profiles/AlbertoBiolchi/certificates/5cadfefe5f1de806e9704577",
+    name: 'CodeAcademy Build a Website with HTML, CSS, and GitHub Pages Skill Path',
+    url: 'https://www.codecademy.com/profiles/AlbertoBiolchi/certificates/5cadfefe5f1de806e9704577',
   },
   {
-    name: "CodeAcademy Building Interactive JavaScript Websites",
-    url: "https://www.codecademy.com/profiles/AlbertoBiolchi/certificates/36ae898a1d1c8524815305b2d1d2ebab",
+    name: 'CodeAcademy Building Interactive JavaScript Websites',
+    url: 'https://www.codecademy.com/profiles/AlbertoBiolchi/certificates/36ae898a1d1c8524815305b2d1d2ebab',
   },
   {
-    name: "CodeAcademy Learn JavaScript Unit Testing",
-    url: "https://www.codecademy.com/profiles/AlbertoBiolchi/certificates/f4276e8c08a3ba174ef0147deb0c3013",
+    name: 'CodeAcademy Learn JavaScript Unit Testing',
+    url: 'https://www.codecademy.com/profiles/AlbertoBiolchi/certificates/f4276e8c08a3ba174ef0147deb0c3013',
   },
   {
-    name: "CodeAcademy Learn HTML",
-    url: "https://www.codecademy.com/profiles/AlbertoBiolchi/certificates/9eb0741e5ebef1f9f58a53bfac67d3a7",
+    name: 'CodeAcademy Learn HTML',
+    url: 'https://www.codecademy.com/profiles/AlbertoBiolchi/certificates/9eb0741e5ebef1f9f58a53bfac67d3a7',
   },
   {
-    name: "CodeAcademy Learn CSS",
-    url: "https://www.codecademy.com/profiles/AlbertoBiolchi/certificates/9a5bb1fc45b4281af1fffec93b0aaf05",
+    name: 'CodeAcademy Learn CSS',
+    url: 'https://www.codecademy.com/profiles/AlbertoBiolchi/certificates/9a5bb1fc45b4281af1fffec93b0aaf05',
   },
   {
-    name: "CodeAcademy Learn JavaScript",
-    url: "https://www.codecademy.com/profiles/AlbertoBiolchi/certificates/705dcb15de0da4dd9d9fc4f3274b430e",
+    name: 'CodeAcademy Learn JavaScript',
+    url: 'https://www.codecademy.com/profiles/AlbertoBiolchi/certificates/705dcb15de0da4dd9d9fc4f3274b430e',
   },
 ];
 
@@ -134,7 +138,7 @@ export const bio = [
       Frameworks:
       -React.js
       `,
-    lang: "ita",
+    lang: 'ita',
   },
   {
     description: `
@@ -147,6 +151,6 @@ export const bio = [
       React.js.
 
     `,
-    lang: "eng",
+    lang: 'eng',
   },
 ];
